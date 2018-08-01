04cc0695f2c5c715fdc94df9e83a15e8098fde0c
mjarkk <mkopenga@gmail.com>
2018-08-01 06:45:46 +0200

    added creating release zip script to buildRelease.sh

--- scripts/buildRelease.sh
+++ scripts/buildRelease.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# check if all commands are supported
+if ! [ -x "$(command -v go)" ]; then
+  echo 'Error: Golang not installed read the README.md for more info' >&2
+  exit 1
+fi
+
+# run the build setup
 sh ./buildSetup.sh
 
 # Copy the example config file
 cp -f ../config.example.json ../build/config.json
 
+# Create release zip in build folder
+go run __makerelease__/main.go
+
 # echo some notes
-echo "NOTE: You have just created a release version that is ready to be released on github"
-echo "NOTE: If you want to create a new git release create a zip with the config.json and setup.exe from the build dir"
+echo "NOTE: You have just created a release version (build/auto_win_10_conf.zip) that is ready to be released on github"
--- scripts/buildSetup.sh
+++ scripts/buildSetup.sh
@@ -2,6 +2,17 @@
 # The build script to create an .exe from the go files 
 # Use this package when doing devolopment and just want to test the application
 
+# check if all commands are supported
+if ! [ -x "$(command -v go)" ]; then
+  echo 'Error: Golang not installed read the README.md for more info' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v rsrc)" ]; then
+  echo 'Error: rsrc not found try `$ go get github.com/akavel/rsrc`' >&2
+  exit 1
+fi
+
 # Create a manifest file for windows
 rsrc -manifest APP.EXE.manifest -o ../FILE.syso
 
